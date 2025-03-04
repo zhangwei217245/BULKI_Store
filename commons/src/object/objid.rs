3c5e38a54837744b140885cf0a23613da8527c65
Wei Zhang <zhangwei217245@lbl.gov>
2025-03-04 13:30:26 -0500

    remove GxHash

--- commons/src/object/objid.rs
+++ commons/src/object/objid.rs
@@ -1,5 +1,4 @@
-// use fnv::FnvHasher;
-use gxhash::GxHasher;
+use fnv::FnvHasher;
 use lazy_static::lazy_static;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
@@ -79,7 +78,7 @@ impl GlobalObjectId {
     }
 
     pub fn with_vnode_id(object_name: &str, vnode_id: Option<u32>) -> Self {
-        let mut hasher = GxHasher::default();
+        let mut hasher = FnvHasher::default();
         object_name.hash(&mut hasher);
         let name_hash = hasher.finish() as u32;
         let timestamp = SystemTime::now()
