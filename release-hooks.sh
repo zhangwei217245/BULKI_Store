2576be99578b7c58084495768b4febbec08a5d4b
Wei Zhang <zhangwei217245@lbl.gov>
2025-03-08 06:05:29 -0500

    update

--- release-hooks.sh
+++ release-hooks.sh
@@ -4,8 +4,8 @@ update_all_versions() {
     local new_version=$1
     
     # Update pyproject.toml
-    sed -i '' "s/version = \"[0-9.]*\"/version = \"$new_version\"/" ./pyproject.toml
-    git add ./pyproject.toml
+    sed -i '' "s/version = \"[0-9.]*\"/version = \"$new_version\"/" ../pyproject.toml
+    git add ../pyproject.toml
     
     # You could update other files that need version info here too
 }
